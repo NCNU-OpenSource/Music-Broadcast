d9c40a8a3299d5f176e088cc863fec3c6317baaf
Edgar <s104321003@ncnu.edu.tw>
2019-01-13 19:27:26 +0800

    Add files via upload

---
+++ yt_helper.py
@@ -0,0 +1,21 @@
+import pafy
+import vlc
+
+def player_build(url):
+    # url = "https://www.youtube.com/watch?v=wXxre8J0GcA"
+    video = pafy.new(url)
+    best = video.getbest()
+    playurl = best.url
+
+    Instance = vlc.Instance()
+    player = Instance.media_player_new()
+    Media = Instance.media_new(playurl)
+    Media.get_mrl()
+    player.set_media(Media)
+    return player
+    # player.play()
+
+def ytdler(url):
+    video = pafy.new(url)
+    bestaudio = video.getbestaudio()
+    bestaudio.download()
